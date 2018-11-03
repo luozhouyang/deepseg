89c561f418bde9c08b11681597dc1c804cb324cf
luozhouyang <stupidme.me.lzy@gmail.com>
2018-11-03 23:22:25 +0800

    Process hparams

--- deepseg/train.py
+++ deepseg/train.py
@@ -1,9 +1,12 @@
 import argparse
-import tensorflow as tf
-from . import data
-from .model import build_model
-import os
 import json
+import sys
+
+import tensorflow as tf
+
+from deepseg import utils
+from deepseg import vocab, data
+from deepseg.model import build_model
 
 
 def add_arguments(parser):
@@ -12,10 +15,17 @@ def add_arguments(parser):
 
 
 def train(hparams):
-    model = build_model()
-    train_iter = data.build_train_dataset(hparams)
+    feat_vocab_table, _ = vocab.create_vocab_table(hparams.feature_vocab_file)
+    label_vocab_table, _ = vocab.create_vocab_table(hparams.label_vocab_file)
+    train_iter = data.build_train_dataset(feat_vocab_table, label_vocab_table, hparams)
     x, y = train_iter.get_next()
-    val_iter = data.build_validation_dataset(hparams)
+    val_iter = None
+    if hparams.validation_features_file and hparams.validation_labels_file:
+        val_feat_vocab_table, _ = vocab.create_vocab_table(hparams.validation_features_file)
+        val_label_vocab_table, _ = vocab.create_vocab_table(hparams.validation_labels_file)
+        val_iter = data.build_validation_dataset(val_feat_vocab_table, val_label_vocab_table, hparams)
+
+    model = build_model()
     model.fit(x, y,
               batch_size=hparams.batch_size,
               epochs=hparams.epochs,
@@ -23,13 +33,25 @@ def train(hparams):
               validation_steps=hparams.validation_steps)
 
 
+def create_hparams(config_file):
+    utils.check_file_exists(config_file)
+    hp = tf.contrib.training.HParams()
+    with open(config_file, mode="rt", encoding="utf8") as f:
+        for k, v in json.loads(f.read()).items():
+            hp.add_hparam(k, v)
+
+    return hp
+
+
+def main(unused_argv):
+    hparams = create_hparams(ARGS.config_file)
+    train(hparams)
+
+
+ARGS = None
+
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     add_arguments(arg_parser)
-    args, _ = arg_parser.parse_known_args()
-    if not os.path.exists(args.config_file):
-        raise FileNotFoundError("File %s does not exist." % args.config_file)
-    with open(args.config_file) as f:
-        hp = tf.contrib.training.HParams()
-        hp.parse_json(json.loads(f.read()))
-    train(hp)
+    ARGS, unparsed = arg_parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
