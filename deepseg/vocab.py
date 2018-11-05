e875f9294d47e8fb3be29c0ef873630c4def8f1e
luozhouyang <stupidme.me.lzy@gmail.com>
2018-11-05 17:20:46 +0800

    Refactoring vocabs generation

--- deepseg/vocab.py
+++ deepseg/vocab.py
@@ -8,33 +8,38 @@ from tensorflow.python.ops import lookup_ops
 
 from . import utils
 
-OOV_PATTERNS = re.compile("[#!?+\-*/.()（）｛｝{}\[\]【】]+")
+PUNCTUATIONS_PATTERNS = re.compile("[#!?+\-*/.()（）｛｝{}\[\]【】]+")
 
 LABEL_S = "S"
 LABEL_B = "B"
 LABEL_E = "E"
 LABEL_M = "M"
-
-UNK = "<unk>"
-UNK_ID = 0
+UNK_LABEL = "U"
+UNK_LABEL_ID = 0
+UNK_CHARS = "<unk>"
+UNK_CHARS_ID = 0
 
 
 def word_filter_callback(word):
-    return not OOV_PATTERNS.findall(word)
+    return not PUNCTUATIONS_PATTERNS.findall(word)
 
 
-DEFAULT_GENERATE_FEATURE_CALLBACKS = [word_filter_callback]
+def oov_filter_callback(word):
+    return UNK_CHARS != word
 
 
-def generate_feature_vocab(feat_file,
-                           vocab_file,
-                           vocab_limit,
-                           oov_token=UNK,
-                           callbacks=DEFAULT_GENERATE_FEATURE_CALLBACKS):
-    if os.path.exists(vocab_file):
-        return
+def unk_label_callback(word):
+    return word in ['S', 'B', 'E', 'M']
+
+
+DEFAULT_GENERATE_FEATURE_CALLBACKS = [word_filter_callback, oov_filter_callback]
+DEFAULT_GENERATE_LABELS_CALLBACKS = [unk_label_callback]
+
+
+def generate_vocab(file, limit=None, callbacks=None):
+    utils.check_file_exists(file)
     counter = Counter()
-    with codecs.getreader("utf8")(tf.gfile.GFile(feat_file, "rb")) as f:
+    with codecs.getreader("utf8")(tf.gfile.GFile(file, "rb")) as f:
         for line in f:
             if not line:
                 continue
@@ -55,21 +60,35 @@ def generate_feature_vocab(feat_file,
                             break
                 if keep:
                     counter[w] += 1
-    c = counter.most_common(vocab_limit)
+    c = counter.most_common(limit)
     vocabs = set()
     for item in c:
         vocabs.add(item[0])
-    if oov_token in vocabs:
-        vocabs.remove(oov_token)
     vocabs = sorted(vocabs)
+    return vocabs
+
+
+def generate_feature_vocab(feat_file,
+                           vocab_file,
+                           vocab_limit=None,
+                           callbacks=DEFAULT_GENERATE_FEATURE_CALLBACKS):
+    vocabs = generate_vocab(feat_file, vocab_limit, callbacks)
     with codecs.getwriter("utf8")(tf.gfile.GFile(vocab_file, "wb")) as w:
         # write UNK first to make sure UNK_ID is 0
-        w.write(UNK + "\n")
+        w.write(UNK_CHARS + "\n")
         for v in vocabs:
             w.write("%s\n" % v)
 
 
-def generate_label_vocab(vocab_file):
+def generate_label_vocab(labels_file, vocab_file, vocab_limit=None, callbacks=DEFAULT_GENERATE_LABELS_CALLBACKS):
+    vocabs = generate_vocab(labels_file, vocab_limit, callbacks)
+    with codecs.getwriter("utf8")(tf.gfile.GFile(vocab_file, "wb")) as w:
+        w.write(UNK_LABEL + "\n")
+        for v in vocabs:
+            w.write("%s\n" % v)
+
+
+def generate_label_vocab_default(vocab_file):
     if os.path.exists(vocab_file):
         return
     with codecs.getwriter("utf8")(tf.gfile.GFile(vocab_file, "wb")) as f:
@@ -79,7 +98,22 @@ def generate_label_vocab(vocab_file):
         f.write(LABEL_E + "\n")
 
 
-def create_vocab_table(vocab_file):
-    word2idx = lookup_ops.index_table_from_file(vocab_file, default_value=UNK_ID)
-    idx2word = lookup_ops.index_to_string_table_from_file(vocab_file, default_value=UNK)
+def create_features_vocab_table(vocab_file):
+    word2idx = lookup_ops.index_table_from_file(vocab_file, default_value=UNK_CHARS_ID)
+    idx2word = lookup_ops.index_to_string_table_from_file(vocab_file, default_value=UNK_CHARS)
     return word2idx, idx2word
+
+
+def create_labels_vocab_table(vocab_file):
+    word2idx = lookup_ops.index_table_from_file(vocab_file, default_value=UNK_LABEL_ID)
+    idx2word = lookup_ops.index_to_string_table_from_file(vocab_file, default_value=UNK_LABEL)
+    word2idx.lookup()
+    return word2idx, idx2word
+
+
+def load_vocab(file):
+    vocabs = []
+    with open(file, mode="rt", encoding="utf8") as f:
+        for line in f:
+            vocabs.append(line.strip("\n").strip())
+    return vocabs
--- deepseg/vocab_test.py
+++ deepseg/vocab_test.py
@@ -13,17 +13,17 @@ class VocabTest(tf.test.TestCase):
     def testGenerateLabelVocab(self):
         feat_file = "testdata/label.txt"
         feat_vocab_file = "testdata/vocab.label.txt"
-        vocab.generate_label_vocab(feat_vocab_file)
+        vocab.generate_label_vocab(feat_file, feat_vocab_file)
 
     def testCreateVocabTable(self):
         feat_vocab_file = "testdata/vocab.feature.txt"
-        word2idx, idx2word = vocab.create_vocab_table(feat_vocab_file)
+        word2idx, idx2word = vocab.create_features_vocab_table(feat_vocab_file)
         with self.test_session() as sess:
             sess.run(tf.tables_initializer())
-            idx = word2idx.lookup(tf.constant(vocab.UNK, dtype=tf.string))
+            idx = word2idx.lookup(tf.constant(vocab.UNK_CHARS, dtype=tf.string))
             self.assertEqual(0, sess.run(idx))
-            word = idx2word.lookup(tf.constant(vocab.UNK_ID, dtype=tf.int64))
-            self.assertEqual(vocab.UNK, sess.run(word).decode("utf8"))
+            word = idx2word.lookup(tf.constant(vocab.UNK_CHARS_ID, dtype=tf.int64))
+            self.assertEqual(vocab.UNK_CHARS, sess.run(word).decode("utf8"))
 
 
 if __name__ == "__main__":
