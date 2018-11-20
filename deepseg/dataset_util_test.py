542ec0a78ed0a14a6f846e8bedc595bd1689ad93
luozhouyang <stupidme.me.lzy@gmail.com>
2018-11-20 16:52:49 +0800

    Update test

--- deepseg/dataset_util_test.py
+++ deepseg/dataset_util_test.py
@@ -1,21 +1,23 @@
+import os
+
 import tensorflow as tf
 from tensorflow.python.framework.errors_impl import OutOfRangeError
 
 from . import dataset_util
-import os
 
 
 class DatasetUtilTest(tf.test.TestCase):
 
     @staticmethod
     def getParams():
-        testdata_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../testdata"))
+        testdata_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "../testdata"))
         params = {
             "model_dir": os.path.join(testdata_dir, "model"),
             "train_src_file": os.path.join(testdata_dir, "feature.txt"),
             "train_tag_file": os.path.join(testdata_dir, "label.txt"),
-            "eval_src_file": os.path.join(testdata_dir, "feature_eval.txt"),
-            "eval_tag_file": os.path.join(testdata_dir, "label_eval.txt"),
+            "eval_src_file": os.path.join(testdata_dir, "feature.txt"),
+            "eval_tag_file": os.path.join(testdata_dir, "label.txt"),
             "predict_src_file": os.path.join(testdata_dir, "feature.txt"),
             "src_vocab": os.path.join(testdata_dir, "vocab.feature.txt"),
             "tag_vocab": os.path.join(testdata_dir, "vocab.label.txt"),
@@ -43,49 +45,38 @@ class DatasetUtilTest(tf.test.TestCase):
         return params
 
     def testBuildTrainDataset(self):
-        dataset = dataset_util.build_train_dataset(self.getParams())
-        iterator = dataset.make_initializable_iterator()
-
         with self.test_session() as sess:
-            sess.run(iterator.initializer)
             try:
                 while True:
-                    (src, src_len) = iterator.get_next()
-                    src = sess.run(src)
-                    src_len = sess.run(src_len)
-                    print(src, src_len)
+                    results = dataset_util.build_train_dataset(self.getParams())
+                    (src, src_len), tag = sess.run(results)
+                    for i, j, k in zip(src, src_len, tag):
+                        print(i, j, k)
             except OutOfRangeError as e:
-                return
+                pass
 
     def testBuildEvalDataset(self):
-        dataset = dataset_util.build_eval_dataset(self.getParams())
-        iterator = dataset.make_initializable_iterator()
-
         with self.test_session() as sess:
-            sess.run(iterator.initializer)
             try:
                 while True:
-                    (src, src_len) = iterator.get_next()
-                    src = sess.run(src)
-                    src_len = sess.run(src_len)
-                    print(src, src_len)
+                    results = dataset_util.build_eval_dataset(self.getParams())
+                    (src, src_len), tag = sess.run(results)
+                    for i, j, k in zip(src, src_len, tag):
+                        print(i, j, k)
             except OutOfRangeError as e:
-                return
+                pass
 
     def testBuildPredictDataset(self):
-        dataset = dataset_util.build_predict_dataset(self.getParams())
-        iterator = dataset.make_initializable_iterator()
-
         with self.test_session() as sess:
-            sess.run(iterator.initializer)
             try:
                 while True:
-                    (src, src_len) = iterator.get_next()
-                    src = sess.run(src)
-                    src_len = sess.run(src_len)
-                    print(src, src_len)
+                    results = dataset_util.build_predict_dataset(
+                        self.getParams())
+                    (src, src_len) = sess.run(results[0])
+                    for i, j in zip(src, src_len):
+                        print(i, j)
             except OutOfRangeError as e:
-                return
+                pass
 
 
 if __name__ == "__main__":
