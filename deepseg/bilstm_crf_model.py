77046aa970e7cc42bb56c3facb70b37107809e9b
luozhouyang <stupidme.me.lzy@gmail.com>
2018-11-20 13:45:33 +0800

    Format file

--- deepseg/bilstm_crf_model.py
+++ deepseg/bilstm_crf_model.py
@@ -9,17 +9,22 @@ class BiLSTMCRFModel(BaseModel):
     def decode(self, output, nwords, params):
         logits = tf.layers.dense(output, params['num_tags'])
         with tf.variable_scope("crf", reuse=tf.AUTO_REUSE):
-            variable = tf.get_variable("transition", shape=[params['num_tags'], params['num_tags']],
-                                       dtype=tf.float32)
+            variable = tf.get_variable(
+                "transition",
+                shape=[params['num_tags'], params['num_tags']],
+                dtype=tf.float32)
             predict_ids, _ = tf.contrib.crf.crf_decode(logits, variable, nwords)
         return logits, predict_ids
 
     def compute_loss(self, logits, labels, nwords, params):
-        tags_str2idx = lookup_ops.index_table_from_file(params['tag_vocab'], default_value=0)
+        tags_str2idx = lookup_ops.index_table_from_file(
+            params['tag_vocab'], default_value=0)
         actual_ids = tags_str2idx.lookup(labels)
         with tf.variable_scope("crf", reuse=True):
-            trans_val = tf.get_variable("transition", shape=[params['num_tags'], params['num_tags']],
-                                        dtype=tf.float32)
+            trans_val = tf.get_variable(
+                "transition",
+                shape=[params['num_tags'], params['num_tags']],
+                dtype=tf.float32)
         log_likelihood, _ = tf.contrib.crf.crf_log_likelihood(
             inputs=logits,
             tag_indices=actual_ids,
