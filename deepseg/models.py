bfbf2a54ff8d6a41def7f0e1dd2c2ddcc6d4a5ea
luozhouyang <zhouyang.luo@gmail.com>
2021-01-12 21:28:26 +0800

    Add models

--- deepseg/models.py
+++ deepseg/models.py
@@ -1,80 +1,46 @@
+import argparse
+import os
+
 import tensorflow as tf
 import tensorflow_addons as tfa
-from tensorflow_addons.text.crf import crf_log_likelihood
-
-from .crf import CRF
+from keras_crf import CRF, CRFAccuracy, CRFLoss
 
 
-def unpack_data(data):
-    if len(data) == 2:
-        return data[0], data[1], None
-    elif len(data) == 3:
-        return data
-    else:
-        raise TypeError("Expected data to be a tuple of size 2 or 3.")
+def build_bilstm_crf_model(vocab_size, embedding_size, hidden_size=256, num_class=3, lr=5e-5):
+    sequence_input = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name='sequence_input')
+    sequence_mask = tf.keras.layers.Lambda(lambda x: tf.greater(x, 0))(sequence_input)
+    embedding = tf.keras.layers.Embedding(vocab_size, embedding_size)(sequence_input)
+    bilstm = tf.keras.layers.Bidirectional(
+        tf.keras.layers.LSTM(hidden_size // 2, return_sequences=True))
+    outputs = bilstm(embedding)
+    crf = CRF(num_class)
+    outputs = crf(outputs, mask=sequence_mask)
+    model = tf.keras.Model(inputs=sequence_input, outputs=outputs)
+    model.compile(
+        loss=CRFLoss(crf),
+        metrics=[
+            CRFAccuracy(crf, name='accuracy'),
+        ],
+        optimizer=tf.keras.optimizers.Adam(lr))
+    model.summary()
+    return model
 
 
-class CRFEndpoint(tf.keras.Model):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def call(self, inputs):
-        raise NotImplementedError()
-
-    def _compute_loss(self, x, y, sample_weight, training=False):
-        y_pred, potentials, sequence_length, chain_kernel = self(x, training=True)
-        crf_loss = -crf_log_likelihood(potentials, y, sequence_length, chain_kernel)[0]
-        if sample_weight:
-            crf_loss = crf_loss * sample_weight
-        crf_loss = tf.reduce_mean(crf_loss)
-        # loss = self.compiled_loss(
-        #     tf.cast(y, dtype=self.dtype),
-        #     tf.cast(y_pred, dtype=self.dtype),
-        #     sample_weight=sample_weight,
-        #     regularization_losses=self.losses)
-        # return crf_loss, loss, y_pred
-        return crf_loss, sum(self.losses), y_pred
-
-    def train_step(self, data):
-        x, y, sample_weight = unpack_data(data)
-        with tf.GradientTape() as tape:
-            crf_loss, loss, y_pred = self._compute_loss(x, y, sample_weight, training=True)
-            total_loss = crf_loss + loss
-
-        gradients = tape.gradient(total_loss, self.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-        self.compiled_metrics.update_state(y, y_pred)
-
-        res = {"crf_loss": crf_loss, 'interal_loss': loss}
-        for m in self.metrics:
-            res[m.name] = m.result()
-        return res
-
-    def test_step(self, data):
-        x, y, sample_weight = unpack_data(data)
-        crf_loss, internal_loss, y_pred = self._compute_loss(x, y, sample_weight)
-        self.compiled_metrics.update_state(y, y_pred)
-        res = {"crf_loss": crf_loss, 'interal_loss': internal_loss}
-        for m in self.metrics:
-            res[m.name] = m.result()
-        return res
-
-
-class BiLSTMCRFModel(CRFEndpoint):
-
-    def __init__(self, vocab_size, embedding_size, hidden_size, num_tags, dropout_rate=0.2, **kwargs):
-        super().__init__(**kwargs)
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_size, name='embedding')
-        self.embedding_dropout = tf.keras.layers.Dropout(dropout_rate)
-        self.bilstm = tf.keras.layers.Bidirectional(
-            tf.keras.layers.LSTM(hidden_size, return_sequences=True), name='bilstm')
-        self.bilstm_dropout = tf.keras.layers.Dropout(dropout_rate)
-        self.crf = CRF(num_tags)
-
-    def call(self, inputs, training=None):
-        seq = inputs
-        outputs = self.embedding_dropout(self.embedding(seq), training=training)
-        outputs = self.bilstm_dropout(self.bilstm(outputs), training=training)
-        outputs = self.crf(outputs)
-        return outputs
+def build_bigru_crf_model(vocab_size, embedding_size, hidden_size=256, num_class=3, lr=5e-5):
+    sequence_input = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name='sequence_input')
+    sequence_mask = tf.keras.layers.Lambda(lambda x: tf.greater(x, 0))(sequence_input)
+    embedding = tf.keras.layers.Embedding(vocab_size, embedding_size)(sequence_input)
+    bigru = tf.keras.layers.Bidirectional(
+        tf.keras.layers.GRU(hidden_size // 2, return_sequences=True))
+    outputs = bigru(embedding)
+    crf = CRF(num_class)
+    outputs = crf(outputs)
+    model = tf.keras.Model(inputs=sequence_input, outputs=outputs)
+    model.compile(
+        loss=CRFLoss(crf),
+        metrics=[
+            CRFAccuracy(crf, name='accuracy')
+        ],
+        optimizer=tf.keras.optimizers.Adam(lr))
+    model.summary()
+    return model
---
+++ tests/models_test.py
@@ -0,0 +1,42 @@
+import unittest
+
+import numpy as np
+from deepseg.dataset import DatasetBuilder, LabelMapper, TokenMapper
+from deepseg.models import build_bigru_crf_model, build_bilstm_crf_model
+
+token_mapper = TokenMapper(vocab_file='testdata/vocab_small.txt')
+label_mapper = LabelMapper()
+
+
+class ModelsTest(unittest.TestCase):
+
+    def _build_inputs(self):
+        builder = DatasetBuilder(token_mapper, label_mapper)
+        train_dataset = builder.build_train_dataset('testdata/train_small.txt', batch_size=20, buffer_size=100)
+        pred_dataset = builder.build_predict_dataset('testdata/test_small.txt', batch_size=10)
+        return train_dataset, pred_dataset
+
+    def test_bilstm_crf_model(self):
+        model = build_bilstm_crf_model(100, 128, 3)
+
+        train_dataset, pred_dataset = self._build_inputs()
+        print(next(iter(train_dataset)))
+        model.fit(train_dataset)
+
+        for p in model.predict(pred_dataset, steps=1):
+            e = np.argmax(p, axis=-1).tolist()
+            print(label_mapper.decode(e))
+
+    def test_bigru_crf_model(self):
+        model = build_bigru_crf_model(100, 128, 3)
+        train_dataset, pred_dataset = self._build_inputs()
+        print(next(iter(train_dataset)))
+        model.fit(train_dataset)
+
+        for p in model.predict(pred_dataset, steps=1):
+            e = np.argmax(p, axis=-1).tolist()
+            print(label_mapper.decode(e))
+
+
+if __name__ == "__main__":
+    unittest.main()
